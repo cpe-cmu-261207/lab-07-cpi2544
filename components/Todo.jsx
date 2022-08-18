bdc6d75daa52d9f29ab5654d9cd469babf8bd0df
Your NameCpi2544 <pokpong1789@gmail.com>
2022-08-18 17:33:28 +0700

    completed

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -7,5 +7,54 @@ import {
 } from "@tabler/icons";
 
 export default function Todo(props) {
-  return <div>your code here...</div>;
+  const [IsMouseOver, setIsMouseOver] = useState(false);
+  const handlemover = () => {
+    setIsMouseOver(true);
+  };
+  const handlemout = () => {
+    setIsMouseOver(false);
+  };
+  return (
+    <div>
+      <div
+        className="border-bottom p-1 py-2 fs-2 d-flex gap-2"
+        onMouseOver={handlemover}
+        onMouseOut={handlemout}
+      >
+        <span
+          style={
+            props.completed
+              ? { textDecoration: "line-through" }
+              : { textDecoration: "" }
+          }
+          className="me-auto"
+        >
+          {props.title}
+        </span>
+
+        {IsMouseOver && (
+          <>
+            <button className="btn btn-success" onClick={() => props.onMark()}>
+              <IconCheck />
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => props.onMoveUp()}
+            >
+              <IconArrowUp />
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => props.onMoveDown()}
+            >
+              <IconArrowDown />
+            </button>
+            <button className="btn btn-danger" onClick={() => props.onDelete()}>
+              <IconTrash />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,62 @@ import {
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+  const [todos, setTodo] = useState([]);
+  const [isFirstRender, setIsfirstRender] = useState(true);
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsfirstRender(false);
+      return;
+    }
+    const todosStr = JSON.stringify(todos);
+    localStorage.setItem("todo-react", todosStr);
+  }, [todos]);
 
-  const markTodo = (idx) => {};
+  useEffect(() => {
+    const todoStr = localStorage.getItem("todo-react");
+    if (!todoStr) setTodo([]);
+    else setTodo(JSON.parse(todoStr));
+  }, []);
+  const deleteTodo = (idx) => {
+    todos.splice(idx, 1);
+    setTodo([...todos]);
+  };
 
-  const moveUp = (idx) => {};
+  const markTodo = (idx) => {
+    todos[idx].completed = !todos[idx].completed;
+    setTodo([...todos]);
+  };
 
-  const moveDown = (idx) => {};
+  const moveUp = (idx) => {
+    if (idx == 0) return;
+    let temp = todos[idx];
+    todos[idx] = todos[idx - 1];
+    todos[idx - 1] = temp;
+    setTodo([...todos]);
+  };
+
+  const moveDown = (idx) => {
+    if (idx == todos.length - 1) return;
+    let temp = todos[idx];
+    todos[idx] = todos[idx + 1];
+    todos[idx + 1] = temp;
+    setTodo([...todos]);
+  };
+
+  const addTodo = (title, completed) => {
+    setTodo([{ title: title, completed: completed }, ...todos]);
+  };
+
+  const handleInput = (event) => {
+    if (event.key == "Enter") {
+      if (event.target.value === "") {
+        alert("Todo cannot be empty");
+        return;
+      }
+      addTodo(event.target.value, false);
+      event.target.value = "";
+    }
+  };
 
   return (
     <div>
@@ -28,40 +77,34 @@ export default function Home() {
         <input
           className="form-control mb-1 fs-4"
           placeholder="insert todo here..."
+          onKeyUp={handleInput}
         />
-        {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
-
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
-
+        {todos.map((element, index) => (
+          <Todo
+            key={index}
+            title={element.title}
+            completed={element.completed}
+            onMark={() => markTodo(index)}
+            onDelete={() => deleteTodo(index)}
+            onMoveUp={() => moveUp(index)}
+            onMoveDown={() => moveDown(index)}
+          />
+        ))}
         {/* summary section */}
         <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
+          <span className="text-primary">All ({todos.length}) </span>
+          <span className="text-warning">
+            Pending (
+            {todos.filter((element) => element.completed == false).length}){" "}
+          </span>
+          <span className="text-success">
+            Completed (
+            {todos.filter((element) => element.completed == true).length})
+          </span>
         </p>
-
         {/* Made by section */}
         <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
+          made by Chayayos Inpracha 640610624
         </p>
       </div>
     </div>
